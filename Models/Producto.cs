c6ecbe75196e143f1cc778ac057388b3ab46a748
Nayel22 <nayelsolorzanom213@gmail.com>
2025-02-23 14:11:32 -0600

    Creacion del modelo Producto

---
+++ Models/Producto.cs
@@ -0,0 +1,24 @@
+﻿namespace ProyectoVI.Models
+{
+    public class Producto
+    {
+
+        public int IdProducto { get; set; }
+        public string Nombre { get; set; }
+        public double Precio { get; set; }
+        public int Stock { get; set; }
+
+        // Constructor por defecto
+        public Producto() { }
+
+        // Constructor parametrizado
+        public Producto(int idProducto, string nombre, double precio, int stock)
+        {
+            IdProducto = idProducto;
+            Nombre = nombre;
+            Precio = precio;
+            Stock = stock;
+        }
+
+    }
+}
