07c2854441c16cc09d6fccef4335d8d8a3fa8267
ROLANDO JOSE ROJAS GUTIERREZ <207620705@castrocarazo.ac.cr>
2025-02-23 13:53:30 -0600

    Modelo Ventas

---
+++ Models/Venta.cs
@@ -0,0 +1,20 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ProyectoVI.Models
+{
+    public class Venta: Cliente
+    {
+        private int IdVenta {  get; set; }
+        private DateTime Date { get; set; }
+        private double Total { get; set; }
+        private int IdCliente { get; set; }
+
+        public Venta(int idCliente, int idVenta, DateTime date, double total)
+        {
+            IdCliente = idCliente;
+            IdVenta = idVenta;
+            Date = date;
+            Total = total;
+        }
+    }
+}
