9b6f4e526263921b169cb4fa72f2c8ce24f5a090
ROLANDO JOSE ROJAS GUTIERREZ <207620705@castrocarazo.ac.cr>
2025-02-23 14:16:22 -0600

    Modelo DetalleVenta

---
+++ Models/DetalleVenta.cs
@@ -0,0 +1,22 @@
+﻿namespace ProyectoVI.Models
+{
+    public class DetalleVenta  
+    {
+        private int IdDetalleVenta { get; set; }
+        private int IdVenta { get; set; }
+        private int IdProducto { get; set; }
+        private int Cantidad { get; set; }
+        private double SubTotal {  get; set; }
+
+        public DetalleVenta(int idDetalleVenta, int idVenta, int idProducto, int cantidad, double subTotal)
+        {
+            IdDetalleVenta = idDetalleVenta;
+            IdVenta = idVenta;
+            IdProducto = idProducto;
+            Cantidad = cantidad;
+            SubTotal = subTotal;
+        }
+
+
+    }
+}
