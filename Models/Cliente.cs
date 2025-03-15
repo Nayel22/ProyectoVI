083eac21cd1bfe8c37abb45b45e37d60308cc69d
ROLANDO JOSE ROJAS GUTIERREZ <207620705@castrocarazo.ac.cr>
2025-03-15 14:42:17 -0600

    clientes update

--- Models/Cliente.cs
+++ Models/Cliente.cs
@@ -2,13 +2,17 @@
 {
     public class Cliente
     {
-     
-            public int IdCliente { get; set; }
-            public string Nombre { get; set; }
-            public string Direccion { get; set; }
-            public string Telefono { get; set; }
 
-     }
+        public int IdCliente { get; set; }
+        public string Nombre { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+        public DateTime FechaRegistro { get; set; } = DateTime.Now;
+        public string AdicionadoPor { get; set; }
+        public DateTime? FechaModificacion { get; set; }
+        public string? ModificadoPor { get; set; }
+
+    }
 
 
 
